677616e54a9df510f54474025a3c3f4382739c0b
Zach <ztay90@gmail.com>
2019-06-06 22:28:08 -0400

    no message

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require ('express')
+const serveStatic = require ('serve-static')
+const path = require ('path')
+// Create the express app
+const app = express()
+// Create middleware to handle the seving app
+app.use("/", serveStatic ( path.join (__dirname, '/public') ) )
+// Serve index by default
+app.get('/', function (req, res){
+  res.sendFile(__dirname + ''index.html')
+})
+// Create default port to serve the app
+const port = process.env.PORT | | 5000
+app.listen(port)
+// Log to feedback that this is actually running
+console.log('server started on port ' + port)
